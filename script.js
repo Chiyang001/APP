2f53322e82dfcbd2cfe2517691f9008d2eeaed85
Chiyang001 <91011415+Chiyang001@users.noreply.github.com>
2024-04-14 09:44:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -18,70 +18,9 @@ function toggleLayout() {
             container.classList.toggle('single-column');
         }
         
-function showVideo() {
-    var videoContainer = document.querySelector('.video-container');
-    var watchTutorialButton = document.getElementById('watchTutorialButton');
-    var videoPlayer = document.getElementById('videoPlayer');
-    var videoFrame = document.getElementById('videoFrame');
-    
-    // 设置视频播放器的 src 属性
-    videoFrame.src = "https://b23.tv/bELxAlP";
-    
-    // 隐藏观看教程按钮，显示视频播放器
-    watchTutorialButton.style.display = "none";
-    videoPlayer.style.display = "block";
-}
-
-function showVideo2() {
-    var videoContainer = document.querySelector('.video-container');
-    var watchTutorialButton = document.getElementById('watchTutorialButton2');
-    var videoPlayer = document.getElementById('videoPlayer');
-    var videoFrame = document.getElementById('videoFrame');
-    
-    // 设置视频播放器的 src 属性
-    videoFrame.src = "https://b23.tv/KlIjADS";
-    
-    // 隐藏观看教程按钮，显示视频播放器
-    watchTutorialButton.style.display = "none";
-    videoPlayer.style.display = "block";
-}
-
-function showVideo3() {
-    var videoContainer = document.querySelector('.video-container');
-    var watchTutorialButton = document.getElementById('watchTutorialButton3');
-    var videoPlayer = document.getElementById('videoPlayer');
-    var videoFrame = document.getElementById('videoFrame');
-    
-    // 设置视频播放器的 src 属性
-    videoFrame.src = "https://b23.tv/BV1kF4m1K7Q9";
-    
-    // 隐藏观看教程按钮，显示视频播放器
-    watchTutorialButton.style.display = "none";
-    videoPlayer.style.display = "block";
-}
 
 
-function copyText() {
-  var text = document.getElementById("codeToCopy").innerText; // 获取文本
-  var textarea = document.createElement("textarea"); // 创建textarea元素
-  textarea.value = text; // 设置textarea的值
-  textarea.style.position = "fixed"; // 防止显示在页面上
-  document.body.appendChild(textarea); // 将textarea加入到文档中
-  
-  textarea.focus();
-  textarea.select(); // 选中文本
-  
-  try {
-    var successful = document.execCommand('copy'); // 执行复制操作
-    var msg = successful ? '成功复制到剪贴板' : '复制失败';
-    alert(msg); // 使用alert显示消息
-  } catch (err) {
-    console.log('无法复制', err);
-    alert('复制过程中发生错误'); // 复制失败时显示提示
-  }
-  
-  document.body.removeChild(textarea); // 移除textarea元素
-}
+
 
 function startAnimation(dot) {
 
@@ -112,4 +51,13 @@ document.querySelectorAll('.app').forEach(app => {
             stopAnimation(dot);
         }
     });
+});
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var nav = document.getElementById('nav-list');
+    if (nav.style.left === '0px') {
+        nav.style.left = '-100%';
+    } else {
+        nav.style.left = '0px';
+    }
 });
