cd952c2518513acb9b3b620a232c4942d3c9d78b
是大大帅啊 <166037044+dadashuai7608@users.noreply.github.com>
2024-06-22 19:21:36 +0800

    Update searchlist.js

--- allAPP/searchlist.js
+++ allAPP/searchlist.js
@@ -146,7 +146,7 @@
             } else {
                 filteredProducts.forEach(product => {
                     const listItem = document.createElement('li');
-                    listItem.innerHTML = `<strong>${product.name}</strong> - 描述：${product.description} - <button-search><a href="${product.downloadUrl}" target="_blank"  style="text-decoration: none;" <span class="yellow-text">下载</a></button-search>`;
+                    listItem.innerHTML = `<strong>${product.name}</strong> - 描述：${product.description} - <button-search><a href="${product.downloadUrl}" target="_blank"  style="text-decoration: none;">下载</a></button-search>`;
                     searchResults.appendChild(listItem);
                 });
             }
