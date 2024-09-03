f90dff468904fa69be40fd52a9896e4399416511
是大大帅啊 <166037044+dadashuai7608@users.noreply.github.com>
2024-09-03 13:03:33 +0800

    Update homescript.js

--- homescript.js
+++ homescript.js
@@ -1,17 +1,8 @@
-function downloadApp(downloadUrl) {
-    // 创建一个隐藏的<a>元素
-    var link = document.createElement("a");
-    link.style.display = "none";
-    link.href = downloadUrl;
-    // 可以选择设置下载的文件名
-    // link.download = "app"; 
-    // 将<a>元素添加到文档中
-    document.body.appendChild(link);
-    // 模拟点击<a>元素以触发下载
-    link.click();
-    // 清理添加的<a>元素
-    document.body.removeChild(link);
-}
+
+        
+function downloadApp(url) {
+            window.location.href = url;
+        }
 
 function toggleLayout() {
             var container = document.querySelector('.app-list');
