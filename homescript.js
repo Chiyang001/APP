e96e58468a232f473964c68a07ed1bd0add8f15b
是大大帅啊 <166037044+dadashuai7608@users.noreply.github.com>
2024-09-03 12:57:26 +0800

    Update homescript.js

--- homescript.js
+++ homescript.js
@@ -4,15 +4,13 @@ function downloadApp(downloadUrl) {
     link.style.display = "none";
     link.href = downloadUrl;
     // 可以选择设置下载的文件名
-    // link.download = "app.apk";
+    // link.download = "app"; 
     // 将<a>元素添加到文档中
     document.body.appendChild(link);
     // 模拟点击<a>元素以触发下载
     link.click();
     // 清理添加的<a>元素
     document.body.removeChild(link);
-
-
 }
 
 function toggleLayout() {
@@ -55,7 +53,7 @@ document.querySelectorAll('.app').forEach(app => {
     });
 });
 
-document.getElementById('mobile-menu').addEventListener('click', function() } 
+document.getElementById('mobile-menu').addEventListener('click', function() {
     var nav = document.getElementById('nav-list');
     if (nav.style.left === '0px') {
         nav.style.left = '-100%';
@@ -63,7 +61,6 @@ document.getElementById('mobile-menu').addEventListener('click', function() }
         nav.style.left = '0px';
     }
 });
-
 // joinus彩蛋
  function getMultiLine(f) {
 var lines =f.toString(); 
